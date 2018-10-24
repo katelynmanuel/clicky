d343098e89e5933aa90df1ab986f14ff03b07514
Katelyn Manuel <katelynmanuel@Katelyns-MacBook-Pro.local>
2018-10-23 22:37:55 -0600

    Adding styling

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./ImageCard.css";
 
 const ImageCard = ({handleClick, data: { id, name, image }}) => (
-    <div className="col-md-3">
+    <div className="col-md-3 main">
         <div onClick={() => handleClick(id)} className="card" id="remove">
-            <div className="img">
+            <div className="imgCards">
                 <img alt={name} src={image} id="characterImage" />
             </div>
         </div>
